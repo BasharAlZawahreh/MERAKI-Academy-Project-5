b65dde94aaea54598c2b26b22bc8f84da4d781c8
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-10-08 18:27:53 +0300

    getAllUsers (need to fix a bug)

--- backend/Admin/Controllers/users.js
+++ backend/Admin/Controllers/users.js
@@ -1,22 +1,54 @@
 const connection = require("../../db/db");
 const makeUserAdminById = async (req, res) => {
-    let id = req.params.id;
-    const query = `UPDATE users SET role="Admin" WHERE user_id=${id}`;
-  
-    connection.query(query, (err, result) => {
-      if (result.affectedRows) {
-        res.status(202).json({
-          success: true,
-          message: ` Success User updated`,
-          result: result,
-        });
-      } else {
-        return res.status(404).json({
-          success: false,
-          message: `user => ${id} not found`,
-        });
-      }
-    });
-  };
+  let id = req.params.id;
+  const query = `UPDATE users SET role="Admin" WHERE user_id=${id}`;
 
-  module.exports = {makeUserAdminById} /// newwwwwwwww
+  connection.query(query, (err, result) => {
+    if (result.affectedRows) {
+      res.status(202).json({
+        success: true,
+        message: ` Success User updated`,
+        result: result,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: `user => ${id} not found`,
+      });
+    }
+  });
+};
+
+
+const getAllUsers = (req, res) => {
+  console.log('get all users')
+  const query = `SELECT * FROM users`;
+
+  connection.query(query, (err, result) => {
+    if (!result.length) {
+      return res.status(404).json({
+        success: false,
+        message: `not found any user`,
+      });
+    } else if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: 'all users',
+      result: result,
+    });
+  });
+};
+
+const BlockUserById = (req,res) =>{
+  const id = req.params.id
+  
+
+}
+
+module.exports = { makeUserAdminById, getAllUsers };
--- backend/Admin/router/AdminRouter.js
+++ backend/Admin/router/AdminRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { login } = require("../../Admin/Controllers/login");
-const { makeUserAdminById } = require("../../Admin/Controllers/users");
+const { makeUserAdminById, getAllUsers } = require("../../Admin/Controllers/users");
 
 // define router
 const adminRouter = express.Router();
@@ -9,8 +9,13 @@ const authorization = require('../middlewares/authorization')
 const authentication = require('../middlewares/authentication')
 
 // 			routes
-//post  http://localhost:5000/login/
+//post  http://localhost:5000/admin/login/
 adminRouter.post("/login", login);
+
+//get  http://localhost:5000/admin/users/
+adminRouter.get("/user", authentication, getAllUsers);
+
+//post  http://localhost:5000/admin/makeAdmin/2
 adminRouter.post("/makeAdmin/:id", authentication, authorization("SuperAdmin"), makeUserAdminById);
 
 module.exports = adminRouter;
