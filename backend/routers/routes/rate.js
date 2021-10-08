5db001dabc51401c4828ed2d90e42053c6c7c421
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-10-08 16:40:44 +0300

    Create getRateByCarId  Router

--- backend/routers/routes/rate.js
+++ backend/routers/routes/rate.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const {addToRate}=require("../controllers/rate");
+const {addToRate,getRateByCarId}=require("../controllers/rate");
 const {authentication}=require("../middlewares/authentication");
 const rateRouter = express.Router();
 
 rateRouter.post("/:car_id",authentication,addToRate);
+rateRouter.get("/:car_id",authentication,getRateByCarId);
 
 module.exports=rateRouter;
