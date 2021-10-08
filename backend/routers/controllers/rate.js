8d1e42dd5bd6bea92bbcb21ac42cc4f30f0c25fa
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-10-08 16:38:03 +0300

    Create getRateByCarId  function

--- backend/routers/controllers/rate.js
+++ backend/routers/controllers/rate.js
@@ -34,5 +34,28 @@ const car_id=req.params.car_id
     }
   });
 };
-
-module.exports = {addToRate}
+const getRateByCarId = (req, res) => {
+    const car_id = req.params.car_id;
+    const query = `SELECT * FROM rates WHERE rates.car_id=?`;
+    const data = [car_id];
+    connection.query(query, data, (err, result) => {
+  
+      if (!result.length) {
+        res.status(500).json({
+          success: false,
+          message: `Not found any rate for this car`,
+        });
+      } else if (err) {
+        res.status(404).json({
+          success: false,
+          message: `Server Error`,
+          err: err,
+        });
+      }
+      res.status(201).json({
+        success: true,
+        result: result,
+      });
+    });
+  };
+module.exports = {addToRate,getRateByCarId}
