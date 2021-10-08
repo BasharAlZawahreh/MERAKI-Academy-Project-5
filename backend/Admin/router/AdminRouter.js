d1874250d2b00e4ff7ab2d132f5451d8c59611cc
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-10-08 17:48:42 +0300

    adminRouter

--- backend/Admin/router/AdminRouter.js
+++ backend/Admin/router/AdminRouter.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { login } = require("../../Admin/Controllers/login");
+const { makeUserAdminById } = require("../../Admin/Controllers/users");
 
 // define router
 const adminRouter = express.Router();
 
+const authorization = require('../middlewares/authorization')
+const authentication = require('../middlewares/authentication')
+
 // 			routes
 //post  http://localhost:5000/login/
 adminRouter.post("/login", login);
+adminRouter.post("/makeAdmin/:id", authentication, authorization("SuperAdmin"), makeUserAdminById);
 
 module.exports = adminRouter;
