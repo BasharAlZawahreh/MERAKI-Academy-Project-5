615b6cfe8c93996f5da3f8e839d6bb02f4cb6493
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-10-12 00:14:20 +0300

    Complete  rigester

--- frontEnd/src/components/Auth/Register/register.js
+++ frontEnd/src/components/Auth/Register/register.js
@@ -8,7 +8,21 @@ const Register=()=>{
     const[password,setPassword]=useState()
     const[done,setDone]=useState()
 
-
+    const reg=(e)=>{
+        e.preventDefault()
+        try {
+            
+            axios.post("http://localhost:5000/users",{firstName,lastName,email,password})
+            .then((result)=>{
+                console.log(result);
+                setDone(<div className="succes">the user has been added successfully</div>)
+            }).catch((err)=>{
+                setDone(<div className="wrong">error happen while regeister please try again</div>)
+            })
+        } catch (error) {
+            setDone(<div className="wrong">error happen while regeister please try again</div>)
+        }
+    }
 
 
     return (
@@ -17,33 +31,29 @@ const Register=()=>{
             <div class="signup-content">
                 <div class="signup-form">
                     <h2 class="form-title">Sign up</h2>
-                    <form method="POST" class="register-form" id="register-form">
+                    <form onSubmit={reg} class="register-form">
                         <div class="form-group">
                             <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                            <input type="text" name="name" id="name" placeholder="Your Name"/>
+                            <input type="text" onChange={(e)=>{ serFirstName(e.target.value)}} name="name" id="name" placeholder="First Name"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                            <input type="text" onChange={(e)=>{setLastName(e.target.value)}}  name="name" id="name" placeholder="Last Name"/>
                         </div>
                         <div class="form-group">
                             <label for="email"><i class="zmdi zmdi-email"></i></label>
-                            <input type="email" name="email" id="email" placeholder="Your Email"/>
+                            <input type="email"onChange={(e)=>{setEmail(e.target.value)}} name="email" id="email" placeholder="Your Email"/>
                         </div>
                         <div class="form-group">
                             <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                            <input type="password" name="pass" id="pass" placeholder="Password"/>
+                            <input type="password" onChange={(e)=>{setPassword(e.target.value)}} name="pass" id="pass" placeholder="Password"/>
                         </div>
                         <div class="form-group">
-                            <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                            <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
-                        </div>
-                        <div class="form-group">
-                            <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                            <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
-                        </div>
-                        <div class="form-group form-button">
-                            <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                            <button   class="form-submit">Register</button>
                         </div>
                     </form>
+                    {done}
                 </div>
-                
                 <div class="signup-image">
                     <figure><img src="images/signin-image.jpg" alt="sing up image"/></figure>
                     <a href="#" class="signup-image-link">I am already member</a>
@@ -53,25 +63,13 @@ const Register=()=>{
     </section>
     // <div className="Register">
     // <input type="text" placeholder="firstName"  />
-    // <input type="text" placeholder="lastName" onChange={(e)=>{
-    //     setLastName(e.target.value)
-    // }} />
-    // <input type="email" placeholder="email" onChange={(e)=>{
-    //     setEmail(e.target.value)
-    // }} />
-    // <input type="password" placeholder="password" onChange={(e)=>{
-    //     setPassword(e.target.value)
-    // }} />
+    // <input type="text" placeholder="lastName" />
+    // <input type="email" placeholder="email"  />
+    // <input type="password" placeholder="password"  />
 
     // <button onClick={()=>{
-    //     axios.post("http://localhost:5000/users",{firstName,lastName,email,password}).then((result)=>{
-    //         setDone(<div className="succes">the user has been added successfully</div>)
-    //     }).catch((err)=>{
-    //         setDone(<div className="wrong">error happen while regeister please try again</div>)
-    //     })
-
-    // }} >Register</button>
-    // {done}
+    //      >Register</button>
+    // 
     // </div>
     )
 }
