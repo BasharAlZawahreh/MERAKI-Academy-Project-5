0c812cc574ccda47b5448aeb9b2b95c4f7f03adb
NaifAlzrigat <86929711+NaifAlzrigat@users.noreply.github.com>
2021-10-11 20:03:17 +0300

    Update App.js

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Auth/Login/login';
-import NavBar from './components/NavBar/nav';
 
 export default function App() {
   return (
     <div>
-      <Login/>
-      {/* <NavBar/> */}
+      <p>App</p>
     </div>
   );
 }
