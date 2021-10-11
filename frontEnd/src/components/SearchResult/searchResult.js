6567e936199bb0c4d6c0aa47c946c741b686e307
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-10-11 17:37:21 +0300

    recieve the result in search result component

--- frontEnd/src/components/SearchResult/searchResult.js
+++ frontEnd/src/components/SearchResult/searchResult.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function SearchResult() {
-    
-    return (
-        <div>
-            
-        </div>
-    )
+  const state = useSelector((state) => {
+    // specify which state to subscribe to (state tree => reducer => state name )
+    return {
+      searches: state.searches.searches,
+    };
+  });
+
+  return <div>
+      {state.searches.map((s,i)=>{
+        return  <div key={i}>
+              {s}
+          </div>
+      })}
+  </div>;
 }
 
-export default SearchResult
+export default SearchResult;
