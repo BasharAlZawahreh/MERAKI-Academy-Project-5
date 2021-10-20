1398aaeb2e4664147d6f1fbb447f2d685489104b
NaifAlzrigat <naif.alzrigat@gmail.com>
2021-10-20 23:24:58 +0300

    add some style for admin dashboard

--- frontEnd/src/components/Admin/Dashboard/Dashboard.js
+++ frontEnd/src/components/Admin/Dashboard/Dashboard.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import Reservations from "../Reservations/Reservations";
 import Users from "../Users/Users";
 import Cars from "../Cars/Cars";
-import styles from "./Dashboard.module.css";
+import styles from "../Dashboard.module.css";
 import { useHistory } from "react-router-dom";
+import Statistics from "../Statistcs/Statistics";
 const Dashboard = () => {
-  const [component, setComponent] = useState("users");
+  const [component, setComponent] = useState("Statistics");
   const history = useHistory();
   return (
     <>
@@ -17,75 +18,44 @@ const Dashboard = () => {
         </div>
         <ul className={styles.navlinks}>
           <li>
-            <a href="#" className={styles.active}>
-              {/* <i className='bx bx-grid-alt' ></i> */}
-              <span className={styles.links_name}>Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
+            <a onClick={()=>setComponent("cars")}>
               {/* <i className='bx bx-box' ></i> */}
-              <span className={styles.links_name}>Cars</span>
+              <span  className={styles.links_name}>Cars</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a onClick={()=>setComponent("users")} >
               {/* <i className='bx bx-list-ul' ></i> */}
               <span className={styles.links_name}>Users</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a onClick={()=>setComponent("reservations")}>
               {/* <i className='bx bx-pie-chart-alt-2' ></i> */}
-              <span className={styles.links_name}>Reservations</span>
+              <span  className={styles.links_name}>Reservations</span>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a onClick={()=>setComponent("Statistics")}>
               {/* <i className='bx bx-coin-stack' ></i> */}
-              <span className={styles.links_name}>Admins</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {/* <i className='bx bx-book-alt' ></i> */}
-              <span className={styles.links_name}>Total Cars</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {/* <i className='bx bx-user' ></i> */}
-              <span className={styles.links_name}>Team</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {/* <i className='bx bx-message' ></i> */}
-              <span className={styles.links_name}>Messages</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {/* <i className='bx bx-heart' ></i> */}
-              <span className={styles.links_name}>Favrorites</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {/* <i className='bx bx-cog' ></i> */}
-              <span className={styles.links_name}>Setting</span>
+              <span className={styles.links_name}>Statistics</span>
             </a>
           </li>
           <li className="log_out">
-            <a href="#">
+            <a>
               {/* <i className='bx bx-log-out'></i> */}
-              <span onClick={()=>{
-                localStorage.removeItem("token")
-                history.push("/admin/login");
-
-              }
-                // dispatch(setToken());
-                } className={styles.links_name}>Log out</span>
+              <span
+                onClick={
+                  () => {
+                    localStorage.removeItem("token");
+                    history.push("/admin/login");
+                  }
+                  // dispatch(setToken());
+                }
+                className={styles.links_name}
+              >
+                Log out
+              </span>
             </a>
           </li>
         </ul>
@@ -93,29 +63,38 @@ const Dashboard = () => {
 
       <section className={styles.homesection}>
         <nav>
+          {/* <div className={styles.sidebarbutton}> */}
+            {/* <button onClick={() => setComponent("users")}>Users</button>
+          </div>
           <div className={styles.sidebarbutton}>
-            {/* <i className='bx bx-menu sidebarBtn'></i> */}
-            <span className={styles.dashboard}>Dashboard</span>
+            <button onClick={() => setComponent("cars")}>Cars</button>
           </div>
-          <div className={styles.searchbox}>
-            {/* <input type="text" placeholder="Search..."/> */}
-            {/* <i className='bx bx-search' ></i> */}
+          <div className={styles.searchbox}></div>
+          <div className={styles.sidebarbutton}>
+            <button className="Ourbtn" onClick={() => setComponent("reservations")}>
+              Reservations
+            </button>
           </div>
-          <div className="profile-details">
-            <button onClick={()=>setComponent("users")}>Users</button>
-            <button onClick={()=>setComponent("cars")}>Cars</button>
-            <button onClick={()=>setComponent("reservations")}>Reservations</button>
+          <div className={styles.sidebarbutton}>
+            <button onClick={() => setComponent("Statistics")}>
+              Statistics
+            </button>
           </div>
+          <div className="profile-details"></div> */}
         </nav>
         <div className={styles.homecontent}>
           <div className={styles.overviewboxes}>
-            {component === "users" ? (
+            {component === "Statistics" ? (
+              <Statistics />
+            ) : component === "users" ? (
               <Users />
             ) : component === "cars" ? (
               <Cars />
-            ) :  component === "reservations" ?(
+            ) : component === "reservations" ? (
               <Reservations />
-            ):""}
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </section>
