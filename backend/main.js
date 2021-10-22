d116e3d4a714006e4e1c2eb6ffb55b089f1787ae
BasharAlZawahreh <86913003+BasharAlZawahreh@users.noreply.github.com>
2021-10-22 20:29:48 +0300

    main server

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 require("./db/db.js");
 require("dotenv").config();
 
@@ -14,46 +14,46 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 // import Routers
-const adminRouter=require("./Admin/router/AdminRouter");
-const userRouter=require("./routers/routes/user");
-const loginRouter=require("./routers/routes/login");
-const carRouter =require("./routers/routes/car")
-const reservationRouter=require("./routers/routes/reservation")
-const rateRouter=require("./routers/routes/rate");
-const payRouter=require("./routers/routes/stripe");
+const adminRouter = require("./Admin/router/AdminRouter");
+const userRouter = require("./routers/routes/user");
+const loginRouter = require("./routers/routes/login");
+const carRouter = require("./routers/routes/car");
+const reservationRouter = require("./routers/routes/reservation");
+const rateRouter = require("./routers/routes/rate");
+const payRouter = require("./routers/routes/stripe");
 // Routers
-app.use("/admin",adminRouter);
-app.use("/login",loginRouter);
-app.use("/users",userRouter);
-app.use("/car",carRouter)
-app.use("/reserve",reservationRouter);
-app.use("/rate",rateRouter);
-app.use("/payment",payRouter);
+app.use("/admin", adminRouter);
+app.use("/login", loginRouter);
+app.use("/users", userRouter);
+app.use("/car", carRouter);
+app.use("/reserve", reservationRouter);
+app.use("/rate", rateRouter);
+app.use("/payment", payRouter);
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
+// let onlineUsers = [];
 
-let onlineUsers = [];
+// const addNewUser = (username, socketId) => {
+//   !onlineUsers.some((user) => user.username === username) &&
+//     onlineUsers.push({ username, socketId });
+// };
 
-const addNewUser = (username, socketId) => {
-  !onlineUsers.some((user) => user.username === username) &&
-    onlineUsers.push({ username, socketId });
-};
+// const removeUser = (socketId) => {
+//   onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+// };
 
-const removeUser = (socketId) => {
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-};
+// const getUser = (username) => {
+//   onlineUsers.shift();
+// for (let index = 0; index < onlineUsers.length; index++) {
+//         if(onlineUsers[index].username==username){
+//           console.log(onlineUsers[index]);
+//           return onlineUsers[index]
+//         }
 
-const getUser = (username) => {
-  onlineUsers.shift();
-for (let index = 0; index < onlineUsers.length; index++) {
-        if(onlineUsers[index].username==username){
-          console.log(onlineUsers[index]);
-          return onlineUsers[index]
-        }
-  
-}
-  // return onlineUsers.find((user) => user.username === username);
-};
+// }
+//   // return onlineUsers.find((user) => user.username === username);
+// };
+
 server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
@@ -62,12 +62,15 @@ server.listen(port, () => {
 io.on("connection", (socket) => {
   // `socket.id` is the id assigned to the user that connected
   console.log(`${socket.id} is connected`);
-  socket.on("set_notification",(data)=>{
+
+  socket.on("set_notification", (data) => {
     // console.log("data",data.amount);
     // socket.to("adminId").emit("set_notification",data.content);
-    socket.emit("set_notification",data.amount);
+    socket.broadcast.emit("set_notification2", data);
+    console.log('data from server',data)
   });
+
   socket.on("disconnect", (socket) => {
     console.log(`${socket.id} is disconnected`);
-  })
+  });
 });
